0a4cf44c4cfda80af85a0700dd0da5e5d4daa335
seunggulee1007 <leesg107@naver.com>
2023-07-06 20:09:29 +0900

    hotfix: 불필요한 파일 삭제
    ```
    - redis 사용으로 인한 jdbcTemplate 관련 코드 삭제
    ```

--- src/main/java/com/gg/tgather/chattingservice/modules/chat/repository/chatmessage/ChatMessageJdbcRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gg.tgather.chattingservice.modules.chat.repository.chatmessage;
-
-import com.gg.tgather.chattingservice.modules.chat.dto.ChatMessageDto;
-import java.util.List;
-
-public interface ChatMessageJdbcRepository {
-    void batchInsert(List<ChatMessageDto> chatMessageDtoList);
-
-}
