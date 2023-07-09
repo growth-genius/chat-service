6be6f24d29e73c29e6186e8caec541c0422c229a
seunggulee1007 <leesg107@naver.com>
2023-07-09 15:06:08 +0900

    fix: chat 요청 url 변경

--- src/main/java/com/gg/tgather/chattingservice/infra/config/WebSocketConfig.java
+++ src/main/java/com/gg/tgather/chattingservice/infra/config/WebSocketConfig.java
@@ -1,14 +1,10 @@
 package com.gg.tgather.chattingservice.infra.config;
 
 import com.gg.tgather.chattingservice.infra.properties.CustomProperties;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -29,9 +25,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/chat/sub");
+        registry.enableSimpleBroker("/sub");
 
-        registry.setApplicationDestinationPrefixes("/chat/pub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 
 }
